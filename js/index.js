fd7c308c869c3a17ea058b984c5071e0a46f60ee
JoaoVicPugliesi <mrlanguages62@gmail.com>
2026-03-10 11:25:23 -0300

    applying colors, fonts and images into the screen 1 components

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const main_screen_1 = document.querySelector('.main-screen-1');
 const main_screen_2 = document.querySelector('.main-screen-2');
 
 document.addEventListener('DOMContentLoaded', () => {
-    main_screen_1.classList.add('invisible');
+    main_screen_2.classList.add('invisible');
 });
