bce5919af60ce432ecf749aead80ebd71cb314f6
JoaoVicPugliesi <mrlanguages62@gmail.com>
2026-03-14 15:14:06 -0300

    helpers: Fetch mock endpoint.

--- js/useCases/random_helper_methods/get_listed_places.js
+++ js/useCases/random_helper_methods/get_listed_places.js
@@ -1,8 +1,8 @@
 async function get_listed_places () {
-    const req = await fetch('../data/cities.json');
+    const req = await fetch('https://mockly.me/custom/weather-app/api ');
     const res = await req.json();
     console.log(res);
-    return res;
+    return res.places;
 }
 
 export default get_listed_places;
